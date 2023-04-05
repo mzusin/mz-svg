f96193531a27ff2702ffc90a09cb9473113005c0
Miriam Zusin <miriamzusin@gmail.com>
2023-04-05 14:19:13 +0300

    createPolygon - unit tests

---
+++ test-browser/modules/polygon.test.js
@@ -0,0 +1,259 @@
+QUnit.module('Polygon', (hooks) => {
+
+  QUnit.test('createPolygon with provided points', (assert) => {
+    const points = '0,100 50,25 50,75 100,0';
+
+    const $polygon1 = mzSVG.createPolygon({
+      points,
+    });
+    const $polygon2 = htmlSVGElementFromString(`<polygon points="${ points }"></polygon>`);
+
+    assert.strictEqual($polygon1.isEqualNode($polygon2), true);
+  });
+
+  QUnit.test('createPolygon with provided fill', (assert) => {
+    const fill = 'none';
+
+    const $polygon1 = mzSVG.createPolygon({
+      fill,
+    });
+    const $polygon2 = htmlSVGElementFromString(`<polygon fill="${ fill }"></polygon>`);
+
+    assert.strictEqual($polygon1.isEqualNode($polygon2), true);
+  });
+
+  QUnit.test('createPolygon with provided stroke', (assert) => {
+    const stroke = 'red';
+
+    const $polygon1 = mzSVG.createPolygon({
+      stroke,
+    });
+    const $polygon2 = htmlSVGElementFromString(`<polygon stroke="${ stroke }"></polygon>`);
+
+    assert.strictEqual($polygon1.isEqualNode($polygon2), true);
+  });
+
+  QUnit.test('createPolygon with provided strokeWidth', (assert) => {
+    const strokeWidth = 10;
+
+    const $polygon1 = mzSVG.createPolygon({
+      strokeWidth,
+    });
+    const $polygon2 = htmlSVGElementFromString(`<polygon stroke-width="${ strokeWidth }"></polygon>`);
+
+    assert.strictEqual($polygon1.isEqualNode($polygon2), true);
+  });
+
+  QUnit.test('createPolygon with provided strokeLinecap', (assert) => {
+    const strokeLinecap = 'round';
+
+    const $polygon1 = mzSVG.createPolygon({
+      strokeLinecap,
+    });
+    const $polygon2 = htmlSVGElementFromString(`<polygon stroke-linecap="${ strokeLinecap }"></polygon>`);
+
+    assert.strictEqual($polygon1.isEqualNode($polygon2), true);
+  });
+
+  QUnit.test('createPolygon with provided strokeLinejoin', (assert) => {
+    const strokeLinejoin = 'round';
+
+    const $polygon1 = mzSVG.createPolygon({
+      strokeLinejoin,
+    });
+    const $polygon2 = htmlSVGElementFromString(`<polygon stroke-linejoin="${ strokeLinejoin }"></polygon>`);
+
+    assert.strictEqual($polygon1.isEqualNode($polygon2), true);
+  });
+
+  QUnit.test('createPolygon with provided vectorEffect', (assert) => {
+    const vectorEffect = 'non-scaling-stroke';
+
+    const $polygon1 = mzSVG.createPolygon({
+      vectorEffect,
+    });
+    const $polygon2 = htmlSVGElementFromString(`<polygon vector-effect="${ vectorEffect }"></polygon>`);
+
+    assert.strictEqual($polygon1.isEqualNode($polygon2), true);
+  });
+
+  QUnit.test('createPolygon with provided id', (assert) => {
+    const id = 'my-polygon';
+
+    const $polygon = mzSVG.createPolygon({
+      id,
+    });
+
+    assert.strictEqual($polygon.getAttribute('id'), id, 'creates a polygon with correct "id" attribute');
+  });
+
+  QUnit.test('createPolygon with provided classes', (assert) => {
+    const classes = 'class1 class2';
+
+    const $polygon = mzSVG.createPolygon({
+      classes,
+    });
+
+    assert.strictEqual($polygon.getAttribute('class'), classes, 'creates a polygon with correct "class" attribute');
+  });
+
+  QUnit.test('createPolygon with provided style', (assert) => {
+    const style = 'fill: red; stroke: blue;';
+
+    const $polygon = mzSVG.createPolygon({
+      style,
+    });
+
+    assert.strictEqual($polygon.getAttribute('style'), style, 'creates a polygon with correct "style" attribute');
+  });
+
+  QUnit.test('createPolygon with provided pathLength', (assert) => {
+    const pathLength = '100';
+
+    const $polygon = mzSVG.createPolygon({
+      pathLength,
+    });
+
+    assert.strictEqual($polygon.getAttribute('pathLength'), pathLength, 'creates a polygon with correct "pathLength" attribute');
+  });
+
+  QUnit.test('createPolygon with provided strokeOpacity', (assert) => {
+    const strokeOpacity = '0.5';
+
+    const $polygon = mzSVG.createPolygon({
+      strokeOpacity,
+    });
+
+    assert.strictEqual($polygon.getAttribute('stroke-opacity'), strokeOpacity, 'creates a polygon with correct "stroke-opacity" attribute');
+  });
+
+  QUnit.test('createPolygon with provided strokeDasharray', (assert) => {
+    const strokeDasharray = '10, 5';
+
+    const $polygon = mzSVG.createPolygon({
+      strokeDasharray,
+    });
+
+    assert.strictEqual($polygon.getAttribute('stroke-dasharray'), strokeDasharray, 'creates a polygon with correct "stroke-dasharray" attribute');
+  });
+
+  QUnit.test('createPolygon with provided strokeDashoffset', (assert) => {
+    const strokeDashoffset = '5';
+
+    const $polygon = mzSVG.createPolygon({
+      strokeDashoffset,
+    });
+
+    assert.strictEqual($polygon.getAttribute('stroke-dashoffset'), strokeDashoffset, 'creates a polygon with correct "stroke-dashoffset" attribute');
+  });
+
+  QUnit.test('createPolygon with provided strokeMiterlimit', (assert) => {
+    const strokeMiterlimit = '10';
+
+    const $polygon = mzSVG.createPolygon({
+      strokeMiterlimit,
+    });
+
+    assert.strictEqual($polygon.getAttribute('stroke-miterlimit'), strokeMiterlimit, 'creates a polygon with correct "stroke-miterlimit" attribute');
+  });
+
+  QUnit.test('createPolygon with provided fillOpacity', (assert) => {
+    const fillOpacity = '0.5';
+
+    const $polygon = mzSVG.createPolygon({
+      fillOpacity,
+    });
+
+    assert.strictEqual($polygon.getAttribute('fill-opacity'), fillOpacity, 'creates a polygon with correct "fill-opacity" attribute');
+  });
+
+  QUnit.test('createPolygon with provided fillRule', (assert) => {
+    const fillRule = 'evenodd';
+
+    const $polygon = mzSVG.createPolygon({
+      fillRule,
+    });
+
+    assert.strictEqual($polygon.getAttribute('fill-rule'), fillRule, 'creates a polygon with correct "fill-rule" attribute');
+  });
+
+  QUnit.test('createPolygon with provided filter', (assert) => {
+    const filter = 'url(#blur)';
+
+    const $polygon = mzSVG.createPolygon({
+      filter,
+    });
+
+    assert.strictEqual($polygon.getAttribute('filter'), filter, 'creates a polygon with correct "filter" attribute');
+  });
+
+  QUnit.test('createPolygon with provided mask', (assert)=> {
+    const mask = 'url(#myMask)';
+
+    const $polygon = mzSVG.createPolygon({
+      mask,
+    });
+
+    assert.strictEqual($polygon.getAttribute('mask'), mask);
+  });
+
+  QUnit.test('createPolygon with provided transform', (assert) => {
+    const transform = 'translate(10,20) rotate(30)';
+
+    const $polygon = mzSVG.createPolygon({
+      transform,
+    });
+
+    assert.strictEqual($polygon.getAttribute('transform'), transform);
+  });
+
+  QUnit.test('createPolygon with provided shapeRendering', (assert) => {
+    const shapeRendering = 'geometricPrecision';
+
+    const $polygon = mzSVG.createPolygon({
+      shapeRendering,
+    });
+
+    assert.strictEqual($polygon.getAttribute('shape-rendering'), shapeRendering);
+  });
+
+  QUnit.test('createPolygon with provided clipPath', (assert) => {
+    const clipPath = 'url(#myClipPath)';
+
+    const $polygon = mzSVG.createPolygon({
+      clipPath,
+    });
+
+    assert.strictEqual($polygon.getAttribute('clip-path'), clipPath);
+  });
+
+  QUnit.test('createPolygon with provided clipRule', (assert) => {
+    const clipRule = 'evenodd';
+
+    const $polygon = mzSVG.createPolygon({
+      clipRule,
+    });
+
+    assert.strictEqual($polygon.getAttribute('clip-rule'), clipRule);
+  });
+
+  QUnit.test('createPolygon with provided opacity', (assert) => {
+    const opacity = 0.5;
+
+    const $polygon = mzSVG.createPolygon({
+      opacity,
+    });
+
+    assert.strictEqual($polygon.getAttribute('opacity'), String(opacity));
+  });
+
+  QUnit.test('createPolygon with provided visibility', (assert) => {
+    const visibility = 'hidden';
+
+    const $polygon = mzSVG.createPolygon({
+      visibility,
+    });
+
+    assert.strictEqual($polygon.getAttribute('visibility'), visibility);
+  });
+});
---
+++ test/polygon.test.ts
@@ -0,0 +1,282 @@
+import {
+    createPolygon,
+} from '../src/index-esm';
+import { createNodeDoc } from './core.test';
+
+describe('Polygon: createPolygon', () => {
+
+    test('createPolygon with provided points', () => {
+        const points = '0,100 50,25 50,75 100,0';
+
+        const $polygon = createPolygon({
+            points,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('points')).toStrictEqual(points);
+    });
+
+    test('createPolygon with provided fill', () => {
+        const fill = 'none';
+
+        const $polygon = createPolygon({
+            fill,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('fill')).toStrictEqual(fill);
+    });
+
+    test('createPolygon with provided strokeWidth', () => {
+        const strokeWidth = 10;
+
+        const $polygon = createPolygon({
+            strokeWidth,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('stroke-width')).toStrictEqual(strokeWidth.toString());
+    });
+
+    test('createPolygon with provided stroke', () => {
+        const stroke = 'red';
+
+        const $polygon = createPolygon({
+            stroke,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('stroke')).toStrictEqual(stroke);
+    });
+
+    test('createPolygon with provided strokeLinecap', () => {
+        const strokeLinecap = 'round';
+
+        const $polygon = createPolygon({
+            strokeLinecap,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('stroke-linecap')).toStrictEqual(strokeLinecap);
+    });
+
+    test('createPolygon with provided strokeLinejoin', () => {
+        const strokeLinejoin = 'round';
+
+        const $polygon = createPolygon({
+            strokeLinejoin,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('stroke-linejoin')).toStrictEqual(strokeLinejoin);
+    });
+
+    test('createPolygon with provided vectorEffect', () => {
+        const vectorEffect = 'non-scaling-stroke';
+
+        const $polygon = createPolygon({
+            vectorEffect,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('vector-effect')).toStrictEqual(vectorEffect);
+    });
+
+    test('createPolygon with provided id', () => {
+        const id = 'polygon-1';
+
+        const $polygon = createPolygon({
+            id,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('id')).toStrictEqual(id);
+    });
+
+    test('createPolygon with provided classes', () => {
+        const classes = 'polygon-class-1 polygon-class-2';
+
+        const $polygon = createPolygon({
+            classes,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('class')).toStrictEqual(classes);
+    });
+
+    test('createPolygon with provided style', () => {
+        const style = 'stroke: red; fill: blue;';
+
+        const $polygon = createPolygon({
+            style,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('style')).toStrictEqual(style);
+    });
+
+    test('createPolygon with provided pathLength', () => {
+        const pathLength = '50%';
+
+        const $polygon = createPolygon({
+            pathLength,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('pathLength')).toStrictEqual(pathLength);
+    });
+
+    test('createPolygon with provided strokeOpacity', () => {
+        const strokeOpacity = 0.5;
+
+        const $polygon = createPolygon({
+            strokeOpacity,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('stroke-opacity')).toStrictEqual(strokeOpacity.toString());
+    });
+
+    test('createPolygon with provided strokeDasharray', () => {
+        const strokeDasharray = '5 10';
+
+        const $polygon = createPolygon({
+            strokeDasharray,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('stroke-dasharray')).toStrictEqual(strokeDasharray);
+    });
+
+    test('createPolygon with provided strokeDashoffset', () => {
+        const strokeDashoffset = '10%';
+
+        const $polygon = createPolygon({
+            strokeDashoffset,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('stroke-dashoffset')).toStrictEqual(strokeDashoffset);
+    });
+
+    test('createPolygon with provided strokeMiterlimit', () => {
+        const strokeMiterlimit = 2;
+
+        const $polygon = createPolygon({
+            strokeMiterlimit,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('stroke-miterlimit')).toStrictEqual(strokeMiterlimit.toString());
+    });
+
+    test('createPolygon with provided fillOpacity', () => {
+        const fillOpacity = 0.5;
+
+        const $polygon = createPolygon({
+            fillOpacity,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('fill-opacity')).toStrictEqual(fillOpacity.toString());
+    });
+
+    test('createPolygon with provided fillRule', () => {
+        const fillRule = 'evenodd';
+
+        const $polygon = createPolygon({
+            fillRule,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('fill-rule')).toStrictEqual(fillRule);
+    });
+
+    test('createPolygon with provided filter', () => {
+        const filter = 'url(#drop-shadow)';
+
+        const $polygon = createPolygon({
+            filter,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('filter')).toStrictEqual(filter);
+    });
+
+    test('createPolygon with provided mask', () => {
+        const mask = 'url(#my-mask)';
+
+        const $polygon = createPolygon({
+            mask,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('mask')).toStrictEqual(mask);
+    });
+
+    test('createPolygon with provided transform', () => {
+        const transform = 'translate(50,50)';
+
+        const $polygon = createPolygon({
+            transform,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('transform')).toStrictEqual(transform);
+    });
+
+    test('createPolygon with provided shapeRendering', () => {
+        const shapeRendering = 'crispEdges';
+
+        const $polygon = createPolygon({
+            shapeRendering,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('shape-rendering')).toStrictEqual(shapeRendering);
+    });
+
+    test('createPolygon with provided clipPath', () => {
+        const clipPath = 'url(#my-clip)';
+
+        const $polygon = createPolygon({
+            clipPath,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('clip-path')).toStrictEqual(clipPath);
+    });
+
+    test('createPolygon with provided clipRule', () => {
+        const clipRule = 'evenodd';
+
+        const $polygon = createPolygon({
+            clipRule,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('clip-rule')).toStrictEqual(clipRule);
+    });
+
+    test('createPolygon with provided opacity', () => {
+        const opacity = 0.5;
+
+        const $polygon = createPolygon({
+            opacity,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('opacity')).toStrictEqual(opacity.toString());
+    });
+
+    test('createPolygon with provided visibility', () => {
+        const visibility = 'hidden';
+
+        const $polygon = createPolygon({
+            visibility,
+            document: createNodeDoc(),
+        });
+
+        expect($polygon.getAttribute('visibility')).toStrictEqual(visibility);
+    });
+});
