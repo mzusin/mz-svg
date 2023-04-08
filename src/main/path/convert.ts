99c2c70d8181361e9405a7cd25a897d1189fd039
Miriam Zusin <miriamzusin@gmail.com>
2023-04-08 21:12:17 +0300

    pathDataToRelative

--- src/main/path/convert.ts
+++ src/main/path/convert.ts
@@ -1,9 +1,5 @@
 import { EPathDataCommand, IPathData } from './interfaces';
 
-export const isRelativeCommand = (command: EPathDataCommand) => {
-    return command.toLowerCase() === command;
-};
-
 export const pathDataToRelative = (pathData: IPathData): IPathData => {
 
     const { commands } = pathData;
@@ -13,6 +9,8 @@ export const pathDataToRelative = (pathData: IPathData): IPathData => {
     // current (x, y) pair
     let x = commands[0].params[0];
     let y = commands[0].params[1];
+    let mx = x;
+    let my = y;
 
     // Make first M to be relative
     commands[0].command = EPathDataCommand.MoveToRel;
@@ -20,54 +18,98 @@ export const pathDataToRelative = (pathData: IPathData): IPathData => {
     for(let i = 1; i<commands.length; i++){
         const item = commands[i]
 
-        // if relative ----> skip
-        if(isRelativeCommand(item.command)) continue;
-
         switch (item.command) {
-            case EPathDataCommand.MoveToAbs:
-            case EPathDataCommand.LineToAbs:{
+            case EPathDataCommand.MoveToAbs:{
                 commands[i].params[0] -= x;
                 commands[i].params[1] -= y;
-                x = commands[i].params[0];
-                y = commands[i].params[1];
+
+                mx = commands[i].params[0];
+                my = commands[i].params[1];
+                break;
+            }
+
+            case EPathDataCommand.ClosePathAbs:
+            case EPathDataCommand.ClosePathRel:{
+                x = mx;
+                y = my;
+                break;
+            }
+
+            case EPathDataCommand.LineToAbs:
+            case EPathDataCommand.QuadraticCurveToSmoothAbs:{
+                const savedX = commands[i].params[0];
+                const savedY = commands[i].params[1];
+
+                commands[i].params[0] -= x;
+                commands[i].params[1] -= y;
+
+                x = savedX;
+                y = savedY;
                 break;
             }
 
             case EPathDataCommand.LineToHorizontalAbs:{
+                const savedX = commands[i].params[0];
                 commands[i].params[0] -= x;
-                x = commands[i].params[0];
+                x = savedX;
                 break;
             }
 
             case EPathDataCommand.LineToVerticalAbs:{
+                const savedY = commands[i].params[1];
                 commands[i].params[0] -= y;
-                y = commands[i].params[0];
+                y = savedY;
                 break;
             }
 
-            case EPathDataCommand.CubicCurveToAbs:
-            case EPathDataCommand.CubicCurveToRel:{
-                //parseCommand(6, token.tokenType, isRelative);
+            case EPathDataCommand.CubicCurveToAbs:{
+                const savedX = commands[i].params[4];
+                const savedY = commands[i].params[5];
+
+                // x1 y1 - control point at the beginning of the curve
+                commands[i].params[0] -= x;
+                commands[i].params[1] -= y;
+
+                // x2 y2 - control point at the end of the curve
+                commands[i].params[2] -= x;
+                commands[i].params[3] -= y;
+
+                // At the end of the command, the new current point becomes the final (x,y) coordinate pair used in the poly-bézier.
+                commands[i].params[4] -= x;
+                commands[i].params[5] -= y;
+
+                x = savedX;
+                y = savedY;
                 break;
             }
 
             case EPathDataCommand.CubicCurveToSmoothAbs:
-            case EPathDataCommand.CubicCurveToSmoothRel:
-            case EPathDataCommand.QuadraticCurveToAbs:
-            case EPathDataCommand.QuadraticCurveToRel:{
-                //parseCommand(4, token.tokenType, isRelative);
+            case EPathDataCommand.QuadraticCurveToAbs:{
+                const savedX = commands[i].params[2];
+                const savedY = commands[i].params[3];
+
+                commands[i].params[0] -= x;
+                commands[i].params[1] -= y;
+
+                commands[i].params[2] -= x;
+                commands[i].params[3] -= y;
+
+                x = savedX;
+                y = savedY;
                 break;
             }
 
-            case EPathDataCommand.QuadraticCurveToSmoothAbs:
-            case EPathDataCommand.QuadraticCurveToSmoothRel:{
-                //parseCommand(2, token.tokenType, isRelative);
-                break;
-            }
+            case EPathDataCommand.ArcAbs:{
+                // (rx ry x-axis-rotation large-arc-flag sweep-flag x y)+
+                const savedX = commands[i].params[5];
+                const savedY = commands[i].params[6];
 
-            case EPathDataCommand.ArcAbs:
-            case EPathDataCommand.ArcRel:{
-                //parseCommand(7, token.tokenType, isRelative);
+                // x y
+                commands[i].params[5] -= x;
+                commands[i].params[6] -= y;
+
+                x = savedX;
+                y = savedY;
                 break;
             }
         }
--- test/path-data/convert.test.ts
+++ test/path-data/convert.test.ts
@@ -194,6 +194,186 @@ describe('Path Data Convert', () => {
                 "errors": []
             });
         });
+
+        test('M10 10 C 20 40 40 40 50 10 60 -20 70 -20 90 10', () => {
+            const parsed = parsePath('M10 10 C 20 40 40 40 50 10   60 -20 70 -20 90 10');
+            // should be: M10 10 c 10 30 30 30 40 0 c 10 -30 20 -30 40 0
+            const res = pathDataToRelative(parsed);
+            expect(res).toStrictEqual({
+                "commands": [
+                    {
+                        "command": "m",
+                        "params": [
+                            10,
+                            10
+                        ]
+                    },
+                    {
+                        "command": "c",
+                        "params": [
+                            10,
+                            30,
+                            30,
+                            30,
+                            40,
+                            0,
+                        ]
+                    },
+                    {
+                        "command": "c",
+                        "params": [
+                            10,
+                            -30,
+                            20,
+                            -30,
+                            40,
+                            0,
+                        ]
+                    }
+                ],
+                "errors": []
+            });
+        });
+
+        test('M10 10H40h50', () => {
+            const parsed = parsePath('M10 10H40h50')
+            const res = pathDataToRelative(parsed);
+
+            // M10 10h30 50
+            expect(res).toStrictEqual({
+                "commands": [
+                    {
+                        "command": "m",
+                        "params": [
+                            10,
+                            10
+                        ]
+                    },
+                    {
+                        "command": "h",
+                        "params": [
+                            30
+                        ]
+                    },
+                    {
+                        "command": "h",
+                        "params": [ 50 ]
+                    }
+                ],
+                "errors": []
+            });
+        });
+
+        test('M10 10V40v50', () => {
+            const parsed = parsePath('M10 10V40v50')
+            const res = pathDataToRelative(parsed);
+
+            // M10 10v30 50
+            expect(res).toStrictEqual({
+                "commands": [
+                    {
+                        "command": "m",
+                        "params": [
+                            10,
+                            10
+                        ]
+                    },
+                    {
+                        "command": "v",
+                        "params": [
+                            30
+                        ]
+                    },
+                    {
+                        "command": "v",
+                        "params": [ 50 ]
+                    }
+                ],
+                "errors": []
+            });
+        });
+
+        test('M40 30A20 40 -45 0 1 60 80', () => {
+            const parsed = parsePath('M40 30A20 40 -45 0 1 60 80')
+            const res = pathDataToRelative(parsed);
+
+            // M40 30a20 40-45 0 1 20 50
+            expect(res).toStrictEqual({
+                "commands": [
+                    {
+                        "command": "m",
+                        "params": [
+                            40,
+                            30
+                        ]
+                    },
+                    {
+                        "command": "a",
+                        "params": [
+                            20, 40, -45, 0, 1, 20, 50
+                        ]
+                    },
+                ],
+                "errors": []
+            });
+        });
+
+        test('M10 10 L20 10 L20 20 Z L10 20 L20 20 z L9 9', () => {
+            const parsed = parsePath('M10 10 L20 10 L20 20 Z L10 20 L20 20 z L9 9')
+            const res = pathDataToRelative(parsed);
+
+            // M10 10 l 10 0 0 10 z l 0 10 10 0 z l-1-1
+            expect(res).toStrictEqual({
+                "commands": [
+                    {
+                        "command": "m",
+                        "params": [
+                            10,
+                            10
+                        ]
+                    },
+                    {
+                        "command": "l",
+                        "params": [
+                            10, 0
+                        ]
+                    },
+                    {
+                        "command": "l",
+                        "params": [
+                            0, 10
+                        ]
+                    },
+                    {
+                        "command": "z",
+                        "params": []
+                    },
+                    {
+                        "command": "l",
+                        "params": [
+                            0, 10
+                        ]
+                    },
+                    {
+                        "command": "l",
+                        "params": [
+                            10, 0
+                        ]
+                    },
+                    {
+                        "command": "z",
+                        "params": []
+                    },
+                    {
+                        "command": "l",
+                        "params": [
+                            -1, -1
+                        ]
+                    },
+                ],
+                "errors": []
+            });
+        });
     });
 
 });
