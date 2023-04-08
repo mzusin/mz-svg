127529761ed6c102d8988b7eb6eae56187676265
Miriam Zusin <miriamzusin@gmail.com>
2023-04-08 16:33:33 +0300

    v1.0.2

--- dist/mz-svg.esm.js
+++ dist/mz-svg.esm.js
@@ -1,6 +1,6 @@
 /* 
 mzSVG - the TypeScript-based library for manipulating and animating SVG.
-Version: 1.0.1
+Version: 1.0.2
 Author: Miriam Zusin     
 Contact: miriam.zusin@gmail.com
 Documentation: https://github.com/mzusin/mz-svg
