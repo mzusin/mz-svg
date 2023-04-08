cf188ed7ed7801651f33630188160a96f812f522
Miriam Zusin <miriamzusin@gmail.com>
2023-04-08 12:45:56 +0300

    path data parser

--- test/path-transformation/path-data-parser.test.ts
+++ test/path-transformation/path-data-parser.test.ts
@@ -2148,6 +2148,50 @@ describe('Path Data Parser', () => {
             });
         });
     });
+
+    describe('Combined', () => {
+
+        test(`M 10,30 
+           A 20,20 0,0,1 50,30
+           A 20,20 0,0,1 90,30
+           Q 90,60 50,90
+           Q 10,60 10,30 z`, () => {
+            const res = parseD(`M 10,30 
+           A 20,20 0,0,1 50,30
+           A 20,20 0,0,1 90,30
+           Q 90,60 50,90
+           Q 10,60 10,30 z`);
+            expect(res).toStrictEqual({
+                "commands": [
+                    {
+                        "command": "M",
+                        "params": [ 10, 30 ]
+                    },
+                    {
+                        "command": "A",
+                        "params": [ 20,20, 0,0,1, 50,30 ]
+                    },
+                    {
+                        "command": "A",
+                        "params": [ 20,20, 0,0,1, 90,30 ]
+                    },
+                    {
+                        "command": "Q",
+                        "params": [ 90,60, 50,90 ]
+                    },
+                    {
+                        "command": "Q",
+                        "params": [ 10,60, 10,30 ]
+                    },
+                    {
+                        "command": "z",
+                        "params": []
+                    }
+                ],
+                "errors": []
+            });
+        });
+    });
 });
 
 
